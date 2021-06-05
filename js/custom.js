be0d3ad668fdc0610bb2e74f014d3dd63c854250
KGE TECHNOLOGIES PVT LTD <kgetechnologies@gmail.com>
2021-06-05 16:30:50 +0530

    3

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
 	$(document).ready(function(){
-    $(". loadlater").each(function(index, element){
+    $(".loadlater").each(function(index, element){
         $(element).attr("src", $(element).attr("data-src"));
     });
