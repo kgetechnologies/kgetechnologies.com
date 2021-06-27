4a8c154ddfb721e584be9ff823f2cd66fc210fb7
KGE TECHNOLOGIES PVT LTD <kgetechnologies@gmail.com>
2021-06-27 16:51:36 +0530

    1

--- js/custom.js
+++ js/custom.js
@@ -2,10 +2,11 @@ function includeHTML() {
 var z, i, elmnt, file, xhttp;
 /*loop through a collection of all HTML elements:*/
 z = document.getElementsByTagName("*");
+var domain=window.location.href.replace(window.location.pathname,"");
 for (i = 0; i < z.length; i++) {
   elmnt = z[i];
   /*search for elements with a certain atrribute:*/
-  file = elmnt.getAttribute("w3-include-html");
+  file = domain+elmnt.getAttribute("w3-include-html");
   if (file) {
     /*make an HTTP request using the attribute value as the file name:*/
     xhttp = new XMLHttpRequest();
