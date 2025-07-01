14df50add8b5885c2f57fa07e0a5304a56dc0294
vijayalakshmi280104 <vijayalakshmi@kgetechnologies.com>
2025-07-01 22:38:28 +0530

    clear error

--- Program.cs
+++ Program.cs
@@ -17,13 +17,13 @@ app.UseHttpsRedirection();
 app.UseRouting();
 
 app.UseAuthorization();
-
-app.MapStaticAssets();
+// No MapStaticAssets, just configure static files normally
+app.UseStaticFiles();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 
 app.Run();
