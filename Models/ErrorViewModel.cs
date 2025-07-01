bd8bcabe2a9bb63a27c2317e06c29d21d15bbb62
KGE TECHNOLOGIES PVT LTD <kgetechnologies@gmail.com>
2025-06-30 22:07:03 -0700

    Base Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace kgetechnologies.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
